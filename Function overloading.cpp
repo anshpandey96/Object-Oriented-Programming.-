186ec37c8c897b7a50e2d8d88c615aef57d23438
Ansh pandey <akashakashpandey612@gmail.com>
2025-04-15 10:52:08 +0530

    Create Function overloading.cpp

---
+++ Function overloading.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int sum(int a,int b)
+{
+    return a+b;
+}
+ int sum(int a,int b,int c)
+ {
+     return a+b+c;
+
+ }
+ int main ()
+ {
+     cout<<"sum is: "<<(7,8);
+     cout<<"sum is :"<<(7,8,4);
+     return 0;
+ }
